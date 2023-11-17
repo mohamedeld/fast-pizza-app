87e6330ab91aa99dbf39709dea5c0471328eef60
mohamedeld <mohamed.azoz20010@gmail.com>
2023-11-17 21:04:52 +0200

    add loader from react-router-dom to get data and use useNavigation

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+function Menu() {
+  const menuData = useLoaderData();
+  console.log(menuData);
+  return (
+    <ul>
+      {menuData.map((pizza) => {
+        return <MenuItem pizza={pizza} key={pizza.id} />;
+      })}
+    </ul>
+  );
+}
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+export default Menu;
